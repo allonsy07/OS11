7c4bc3d5f8fc3c7850953408bc07ba29a1c553f6
aiccuracy <soojin.emily@gmail.com>
2022-05-22 19:18:05 +0900

    fix: change list sorting

--- nonpreemptive_priority.py
+++ nonpreemptive_priority.py
@@ -14,7 +14,7 @@ def nonpreemtive_priority(data: List[dict]):
     for i in data:
         priority.append([i["priority"], i["arrival_time"], i["burst_time"], idx])
         idx += 1
-    sorted_priority = sorted(priority, ascending = False)
+    sorted_priority = sorted(priority, key = lambda x: x[0], reverse=False) # priority 숫자가 작을수록 우선순위가 높다.
     while priority:
         for val in priority:
             if arrived(sorted_priority, curr_time, val[-1]):
